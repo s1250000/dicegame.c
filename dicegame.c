a46e0f6766328e954caf51006b2f022ccc9c7909
s1280034 <87128549+s1280034@users.noreply.github.com>
2021-07-08 22:52:05 +0900

    Add files via upload

---
+++ dicegame.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int GetRandom(int min,int max);
+
+int main(){
+  char name[10];
+  printf("what is your name?>");
+  scanf("%s",name);
+  printf("Hello,%s!\n",name);
+    int i;
+    int j;
+    int a[2];
+    
+    srand((unsigned int)time(NULL));
+    a[0]=GetRandom(1,6);
+    a[1]=GetRandom(2,7)-1;
+	printf("Rolling the dice...\n");
+		for (i = 0;i < 2;i++){
+            printf("die %d:",i+1);
+	    printf("%d\n",a[i]);
+        }
+        
+	printf("Total value: %d\n",a[0]+a[1]);
+	if(a[0]+a[1]>7){
+	  printf("%s won\n",name);
+	}
+	 else printf("%s lose\n",name);
+	return 0;
+}
+
+int GetRandom(int min,int max){
+	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
+}
